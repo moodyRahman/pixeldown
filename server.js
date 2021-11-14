09087fd263607ad58f4a82582569ecfcc346fafe
Moody Rahman <moody.byte@gmail.com>
2021-11-14 02:17:50 -0500

    run commands on request

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-
 import express from "express"
+import { exec } from "child_process"
+
 const app = express()
-const port = 3000
+const port = 3005
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/start', (req, res) => {
+    console.log(req.body)
+    exec("screen -S mc_server -p 0 -X stuff \"echo hi im moody ^M\"", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.send("damn something bad happened lol")
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        res.send("command piped in :)")
+    });
 })
 
 app.listen(port, () => {
